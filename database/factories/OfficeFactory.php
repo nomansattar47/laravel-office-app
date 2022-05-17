18e54ef44011dc1dfd7f600db251dd821d2d454e
noman.sattar <nomansattar47@gmail.com>
2022-05-17 15:36:13 +0500

    first commit

---
+++ database/factories/OfficeFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Office;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Office>
+ */
+class OfficeFactory extends Factory
+{
+    protected $model = Office::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'lat' => $this->faker->latitude,
+            'lng' => $this->faker->longitude,
+            'address_line1' => $this->faker->address,
+            'approval_status' => 2,
+            'hidden' => false,
+            'price_per_day' => $this->faker->numberBetween(1_000, 2_000),
+            'monthly_discount' => 0
+        ];
+    }
+}
---
+++ database/factories/ReservationFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Office;
+use App\Models\Reservation;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reservation>
+ */
+class ReservationFactory extends Factory
+{
+    protected $model = Reservation::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'office_id' => Office::factory(),
+            'price' => $this->faker->numberBetween(10_000, 20_000),
+            'status' => 1,
+            'start_date' => now()->addDay(1)->format('Y-m-d'),
+            'end_date' => now()->addDay(1)->format('Y-m-d'),
+        ];
+    }
+}
